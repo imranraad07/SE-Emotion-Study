107406c5babfbf32a6ba513d65b46e4ac62be7d5
imranraad07 <imranraad@gmail.com>
2022-05-06 13:35:29 -0400

    crowlers modified

--- crowlers/githubcrowler.py
+++ crowlers/githubcrowler.py
@@ -1,22 +1,38 @@
+import argparse
 import csv
-import json
-import time
 from datetime import datetime
 
 from github import Github
 import os
 
-token = os.getenv('GITHUB_TOKEN', '...')
+parser = argparse.ArgumentParser()
+parser.add_argument('--github_token', type=str, required=True)
+parser.add_argument('--type', type=str, required=True, help="pull_request or issue")
+parser.add_argument('--repo_name', type=str, required=True)
+parser.add_argument('--output_path', type=str, required=True)
+
+args = parser.parse_args()
+
+github_token = args.github_token
+type = args.type
+repo_name = args.repo_name
+output_path = args.output_path
+
+token = os.getenv('GITHUB_TOKEN', github_token)
 g = Github(token)
 total_count = 5000
-repo = g.get_repo("webpack/webpack")
 
+repo = g.get_repo(repo_name)
 since = datetime.strptime("2020-01-01T00:00:00", "%Y-%m-%dT%H:%M:%S")
 # https://github.com/microsoft/TypeScript
-with open('../dataset/github/pull_requests/webpack.csv', 'w+') as csvfile:
+with open(output_path, 'w+') as csvfile:
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(['id', 'created_at', 'user', 'comment'])
-    issue_comments = repo.get_pulls_comments(direction="desc", sort="updated")
+    if type == "pull_request":
+        issue_comments = repo.get_pulls_comments(direction="desc", sort="updated")
+    else:
+        issue_comments = repo.get_issue_comments(direction="desc", sort="updated")
+
     print(issue_comments.totalCount)
 
     count = 0
@@ -36,3 +52,4 @@ with open('../dataset/github/pull_requests/webpack.csv', 'w+') as csvfile:
                     print("Exception occurred")
         except:
             print("Exception occurred in rest request")
+
